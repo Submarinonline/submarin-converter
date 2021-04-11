bd6be19c2b39d9811ef7c0aac624b1b0913d339b
hakunagi <hakunagi.rei@gmail.com>
2021-04-12 01:23:59 +0900

    パスの指定ミス

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const converter = (function () {
     if (Object.keys(mainModule).includes(val))
       temp[val] = mainModule[val]
     else if (Object.keys(addon).includes(val))
-      temp[val] = new convertModule(addon[val].type, require(addon[val].module), addon[val].role)
+      temp[val] = new convertModule(addon[val].type, require(`${root}/submarin-converter-addons/${addon[val].module}`), addon[val].role)
     else
       throw new Error(`"${val}"は存在しません。settings.jsonのpriorityを編集して下さい。`)
   })
